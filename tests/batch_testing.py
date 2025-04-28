66b05e6a0d30fe378a9806b329cc06b3d0175307
suyttthideptrai <ntienthanhn@gmail.com>
2025-04-28 21:45:04 +0700

    minor fix #3

--- tests/batch_testing.py
+++ tests/batch_testing.py
@@ -72,14 +72,5 @@ def main():
 
 
     
-    # # sample_file = os.path.join(C.FILTERED_DATASET_PATH, "SIREN_us8k_5776.wav")
-    # #dataset/CAR_HORN_us8k_7693.wav
-    # sample_file = os.path.join(C.FILTERED_DATASET_PATH, "CAR_HORN_us8k_7693.wav")
-    # wav_data = load_wav_16k_mono_3(sample_file)
-    # infer = reloaded_model.signatures["serving_default"]
-    # result = infer(wav_data)
-    # # print(result)
-    # tensor_values = result["output_0"]
-    # max_score = tf.reduce_max(tensor_values)
-    # max_index = tf.argmax(tensor_values)
-    # print(f"Max Score: {max_score}, Index: {max_index}")
+if __name__ == "__main__":
+    main()
