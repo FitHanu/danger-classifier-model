c176869421a9ccc1fdfff361ea793b8b8e3e156b
Thành <131802697+suyttthideptrai@users.noreply.github.com>
2025-06-11 18:34:22 +0000

    no batch for input

--- workflow.py
+++ workflow.py
@@ -330,7 +330,7 @@ def train(ds_ts: tf.data.Dataset) -> None:
         #         dtype=tf.float32
         #     )
         #     return embeddings
-            _, embeddings, _ = embedding_extraction_layer(tf.squeeze(inputs))
+            _, embeddings, _ = embedding_extraction_layer(inputs)
             return embeddings
     
     embeddings_output = EmbeddingExtractionLayer()(input_segment)
